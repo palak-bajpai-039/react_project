27f7b952e7c41b3525c2573b07f244d2ed3d1324
palak-bajpai-039 <riturajbajpai76@gmail.com>
2026-07-17 00:29:58 +0530

    react_project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
-import { Todo } from "./components/projects/todo/todo";
+//import { Todo } from "./components/projects/todo/todo";
 //import { Keys } from "./keys";
+import ShortCiruit from "./components/ShortCircuit";
 
 export const App = () => {
-  return <Todo />;
+  return (
+    <section>
+      <ShortCiruit />
+    </section>
+  );
 };
