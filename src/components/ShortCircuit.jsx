7bef9a0aed9aeb84fd405546310b121414cbe07c
palak-bajpai-039 <riturajbajpai76@gmail.com>
2026-07-17 00:59:37 +0530

    react_project

--- src/components/ShortCircuit.jsx
+++ src/components/ShortCircuit.jsx
@@ -1,12 +1,15 @@
 import "/src/index.css";
-
+import { useState } from "react";
 const ShortCircuit = () => {
+  const [isloggedIn, setIsLoggedIn] = useState(true);
   return (
     <section className="container short-container">
       <h1>Welcome to Short Circuit Evaluation</h1>
       {<p>You are logged in!</p>}
       <div className="grid-three-cols">
-        <button>Toggle login State</button>
+        <button onClick={() => setIsLoggedIn(!isloggedIn)}>
+          Toggle login State
+        </button>
         <button>Set User</button>
         <button> Clear User</button>
       </div>
