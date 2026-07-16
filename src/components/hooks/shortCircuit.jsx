f59c2c42b711c0ab19e237544d3e3a159463659b
palak-bajpai-039 <riturajbajpai76@gmail.com>
2026-07-17 00:25:15 +0530

    react_project

---
+++ src/components/hooks/shortCircuit.jsx
@@ -0,0 +1,17 @@
+import "./hooks.css";
+
+const ShortCircuit = () => {
+  return (
+    <section className="container short-container">
+      <h1>Welcome to Short Circuit Evaluation</h1>
+      {<p>You are logged in!</p>}
+      <div className="grid-three-cols">
+        <button>Toggle login State</button>
+        <button>Set User</button>
+        <button> Clear User</button>
+      </div>
+    </section>
+  );
+};
+
+export default ShortCircuit;
