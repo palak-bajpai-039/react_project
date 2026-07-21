44ac1d15b373fc4e43f15221ff4f448e0a9e1443
palak-bajpai-039 <riturajbajpai76@gmail.com>
2026-07-22 00:30:52 +0530

    react_project

---
+++ src/components/hooks/useState/index.jsx
@@ -0,0 +1,13 @@
+import "../";
+export const counter = () => {
+  return (
+    <div
+      className="container state-container"
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <h1>useState Hooks!</h1>
+    </div>
+  );
+};
