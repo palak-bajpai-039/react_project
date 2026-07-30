45700e34c4df5a25da1ac5c6ccf9178f320552c2
palak-bajpai-039 <riturajbajpai76@gmail.com>
2026-07-30 14:24:49 +0530

    react-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./components/EV.css";
-import { RegistrationFrom } from "./components/hooks/useState/registrationFrom";
+import { RegistrationForm } from "./components/hooks/useState/registrationFrom";
 
 //import { Counter } from "./components/hooks/useState";
 //import CounterChallenges from "./components/hooks/useState/challenge";
@@ -7,5 +7,5 @@ import { RegistrationFrom } from "./components/hooks/useState/registrationFrom";
 //import ControlledFrom from "./components/hooks/useState/controlled";
 
 export const App = () => {
-  return <RegistrationFrom />;
+  return <RegistrationForm />;
 };
