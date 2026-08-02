02ed414d9a8dd8180b7c2b1081f8b2ed8c4eed1f
palak-bajpai-039 <riturajbajpai76@gmail.com>
2026-08-02 11:31:07 +0530

    react-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./components/EV.css";
-import NetflixSeries from "./components/NetflixSeries";
-//import { RegistrationReactForm } from "./components/hooks/useState/registrationReactForm";
+//import NetflixSeries from "./components/NetflixSeries";
+import { RegistrationReactForm } from "./components/hooks/useState/registrationReactForm";
 
 //import { Counter } from "./components/hooks/useState";
 //import CounterChallenges from "./components/hooks/useState/challenge";
@@ -8,12 +8,5 @@ import NetflixSeries from "./components/NetflixSeries";
 //import ControlledFrom from "./components/hooks/useState/controlled";
 
 export const App = () => {
-  return (
-    <section>
-      <h1>Top netflixSeries</h1>
-      <br />
-      <br />
-      <NetflixSeries />
-    </section>
-  );
+  return <RegistrationReactForm />;
 };
