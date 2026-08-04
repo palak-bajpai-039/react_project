c70bbefe62aa4d49b7fa073c9c7c5fdba7c1f72c
palak-bajpai-039 <riturajbajpai76@gmail.com>
2026-08-04 23:52:26 +0530

    react-project

--- src/components/hooks/useState/LoginForm.jsx
+++ src/components/hooks/useState/LoginForm.jsx
@@ -1,15 +1,42 @@
 import "./index2.css";
+import { useState } from "react";
 
 export const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [passward, setPassward] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const logindata = {
+      username: username,
+      passward: passward,
+    };
+    console.log(logindata);
+  };
+
   return (
     <div className="container">
       <div className="card">
         <h1>Login Form </h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" required autoComplete="off" />
+          <input
+            type="text"
+            name="username"
+            required
+            autoComplete="off"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
           <label htmlFor="passward">Passward</label>
-          <input type="text" name="passward" required autoComplete="off" />
+          <input
+            type="text"
+            name="passward"
+            required
+            autoComplete="off"
+            value={passward}
+            onChange={(e) => setPassward(e.target.value)}
+          />
 
           <button type="submit">Login</button>
         </form>
