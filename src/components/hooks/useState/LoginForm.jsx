f920ea50c195944bfce6845440b1880047ddb843
palak-bajpai-039 <riturajbajpai76@gmail.com>
2026-08-04 23:39:01 +0530

    react-project

---
+++ src/components/hooks/useState/LoginForm.jsx
@@ -0,0 +1,19 @@
+import "./index.css";
+
+export const LoginForm = () => {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Login Form </h1>
+        <form>
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" required autoComplete="off" />
+          <label htmlFor="passward">Passward</label>
+          <input type="text" name="passward" required autoComplete="off" />
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
